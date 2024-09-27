305a21c3985640b95222a7471a47642c144d1f7e
MicaelNF <micaelnf@ciencias.unam.mx>
2024-09-27 12:10:09 -0600

    Se solucionarion algunos errores de la práctica 2.

--- practicas/Practica02/practica02.hs
+++ practicas/Practica02/practica02.hs
@@ -9,17 +9,20 @@ sumaLista [] = 0
 sumaLista (x : xs) = x + sumaLista(xs)
 
 agregaElemento :: [a] -> a -> Bool -> [a] --Agrega un elemento a una lista ya sea al inicio o al final dependiendo del valor booleano.
-agregaElemento lista elemento True  = elemento : lista
-agregaElemento lista elemento False = lista ++ [elemento]
+agregaElemento lista elemento posInicio = --Reduje la función a un solo caso.
+  if posInicio
+    then elemento : lista
+  else lista ++ [elemento]
 
 maximoLista :: (Num a, Ord a) => [a] -> a --Calcula el valor máximo de una lista
-maximoLista [] = 0
+maximoLista [] = error "Lista está vacia" --Cambie el caso base.
 maximoLista (x : xs) =
   if x > maximoLista(xs)
     then x
   else maximoLista(xs)
 
 indice :: [a] -> Int -> a --Busca la posición en la que se encuntra cierto elemento en una lista a partir de un número entero dado.
+indice [] numero = error "Lista esta vacia" --Agregue un caso base para una lista vacía.
 indice (x : xs) numero =
   if (numero > 0) && (numero <= longitud(xs)) --No reste 1 ya que al quitar un elemento de la lista es como restarle uno.
   then indice(xs) (numero-1)
@@ -31,15 +34,7 @@ indice (x : xs) numero =
 
 divisores :: Int -> [Int] --Esta función calcula y genera una lista de los números que pueden dividir a cierto numero dado.
 divisores 0 = []
-divisores xs = [x | x <- auxiliarDeDivisores(xs), mod xs x == 0]
-
-auxiliarDeDivisores :: Int -> [Int] --Esta es una función que genera una lista que necesita la función "divisores".
-auxiliarDeDivisores x =
-  if x == 0
-    then []
-  else if x > 0
-  then x : auxiliarDeDivisores (x - 1) -- Este caso es para los positivos.
-  else x : auxiliarDeDivisores (x + 1) -- Este caso es para los negativos.
+divisores n = [x | x <- [1..n], mod n x == 0] --Use [1..n] para generar una lista sin depender de una función auxiliar.
 
 conjunto :: Eq a => [a] -> [a] --Esta función genera una lista sin elementos repetidos.
 conjunto [] = []
